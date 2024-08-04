9bfe93132d0abdfc5bdfa04387c35577637057f0
Ilya <sange0337@gmail.com>
2024-08-04 22:01:44 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import moon from "./assets/images/icon-moon.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="bg-overlay h-52 md:h-80"></div>
+            <div className="max-w-lg mx-auto pt-10 md:pt-16">
+                <header className="flex items-center justify-between mb-10 md:mb-12">
+                    <h1 className="ff-bold text-4xl md:text-5xl text-white tracking-superwide">TODO</h1>
+                    <div className="w-6 h-6 md:w-8 md:h-8">
+                        <img src={moon} alt="Moon" className="w-full h-full"/>
+                    </div>
+                </header>
+                <main className="pb-10 md:pb-16">
+                  hello world
+                </main>
+                <p className="text-center text-gray-400 text-sm">Drag and drop to reorder list</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
